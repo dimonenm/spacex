abe4ff5d5f25bc81e80ec700fec9cb24e741ec29
arzuakov@yahoo.com <arzuakov@yahoo.com>
2020-10-20 17:02:27 +0300

    w20.10.2020

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Features from './components/Features/Features';
 import Footer from './components/Footer/Footer';
 // import Calendar from './components/Calendar/Calendar';
 // import Details from './components/Details/Details';
+import FetchData from './service/FetchData.js';
+
 import './style.css';
+
 
 // const app = React.createElement('div', { class: 'App' }, 'hello world!'); // неудобный вариант добавления элементов
 
@@ -22,15 +25,38 @@ import './style.css';
 
 class App extends React.Component {
 
+  fetchData = new FetchData();
+
   state = {
     rocket: 'Falcon 1',
+    rocketFeatures: null,
+    rockets: [],
   };
+
+  componentDidMount() {
+    // this.fetchData.getCompany().then(data => console.log(data));
+    this.updateRocket();
+  }
+
+  updateRocket() {
+    this.fetchData.getRocket()
+      .then(data => {
+        this.setState({ rockets: data.map(item => item.name) });
+        return data;
+      })
+      .then(data => data.find(item => item.name === this.state.rocket))
+      .then(rocketFeatures => this.setState({ rocketFeatures })); // == .then(features => this.setState({rocketFeatures: features}))
+  }
+
+  cangeRocket = (rocket) => {
+    this.setState({ rocket }, this.updateRocket);
+  }
 
   render() {
     return (
       <React.Fragment>
-        <Header />
-        <Main rocket={this.state.rocket}/>
+        <Header rockets={this.state.rockets} cangeRocket={this.cangeRocket}/>
+        <Main rocket={this.state.rocket} />
         <Features />
         <Footer />
       </React.Fragment>
